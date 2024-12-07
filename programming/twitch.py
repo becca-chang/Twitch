f2490b6449a0db03d294a42cbd7a56f8803a0b9d
becca-chang <113356011@g.nccu.edu.tw>
2024-12-08 07:35:17 +0800

    ensure valid file name

--- programming/twitch.py
+++ programming/twitch.py
@@ -539,26 +539,32 @@ def get_clips_without_chats(clip_directory, chat_directory):
 def create_report(messaged_re_dir):
     user_reports_list = []
     for file in os.listdir(messaged_re_dir):
-        full_file_path = os.path.join(messaged_re_dir, file)
-        df = pd.read_csv(full_file_path, index_col=0)
         user_id = file.split(".")[0]
-        message_count = df["message_id"].count()
-        distinct_clip_count = df["clip_id"].nunique()
-        subscribed_count = df[df["subscribed_type"] == 1]["tier_level"].count()
-        gifting_count = df[df["subscribed_type"] == 2]["gifting_count"].count()
-        gifting_amount = int(df[df["subscribed_type"] == 2]["gifting_count"].sum())
-        cheer_count = df[df["subscribed_type"] == 3]["message"].count()
-        cheer_amount = int(df[df["subscribed_type"] == 3]["cheer"].sum())
-        user_report = {
-            "user_id": user_id,
-            "message_count": message_count,
-            "distinct_clip_count": distinct_clip_count,
-            "subscribed_count": subscribed_count,
-            "gifting_count": gifting_count,
-            "gifting_amount": gifting_amount,
-            "cheer_count": cheer_count,
-            "cheer_amount": cheer_amount,
-        }
-        user_reports_list.append(user_report)
+        if user_id.isdigit():
+            full_file_path = os.path.join(messaged_re_dir, file)
+            df = pd.read_csv(full_file_path, index_col=0)
+
+            message_count = df["message_id"].count()
+            distinct_clip_count = df["clip_id"].nunique()
+            subscribed_count = df[df["subscribed_type"] == 1]["tier_level"].count()
+            gifting_count = df[df["subscribed_type"] == 2]["gifting_count"].count()
+            gifting_amount = int(df[df["subscribed_type"] == 2]["gifting_count"].sum())
+            cheer_count = df[df["subscribed_type"] == 3]["message"].count()
+            cheer_amount = int(df[df["subscribed_type"] == 3]["cheer"].sum())
+            user_report = {
+                "user_id": user_id,
+                "message_count": message_count,
+                "distinct_clip_count": distinct_clip_count,
+                "subscribed_count": subscribed_count,
+                "gifting_count": gifting_count,
+                "gifting_amount": gifting_amount,
+                "cheer_count": cheer_count,
+                "cheer_amount": cheer_amount,
+            }
+            user_reports_list.append(user_report)
     report_df = pd.DataFrame(data=user_reports_list)
     report_df.to_csv("data/reports.csv")
+
+
+messaged_re_dir = "data/chats/messaged_re"
+create_report(messaged_re_dir)
